72c7a43057240b6f83e8cfbe6f53e3a93d802dda
miplo <miplotnova@mail.ru>
2024-12-30 17:31:33 +0500

    Homework_23

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')  # Отображение id и name в списке
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'category')  # Отображение id, name, price и category в списке
+    list_filter = ('category',)  # Фильтрация по категории
+    search_fields = ('name', 'description')  # Поиск по полям name и description
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
--- config/settings.py
+++ config/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydatabase',  # Имя вашей базы данных
+        'USER': 'myuser',      # Имя пользователя
+        'PASSWORD': 'mypassword',  # Пароль пользователя
+        'HOST': 'localhost',   # Обычно localhost
+        'PORT': '5432',        # Порт, по умолчанию 5432
     }
 }
 
