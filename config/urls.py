0568a7dcf0603dec609dcf791489ed572bf2abd6
miplo <miplotnova@mail.ru>
2025-01-05 16:41:03 +0500

    Homework_23

--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('catalog/', include('catalog.urls')),  # Подключаем маршруты приложения catalog
+    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),   # Ваши другие URL-адреса
 ]
