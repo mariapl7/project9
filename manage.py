c309913a653c32ce8c3f44a073ea24b2d0cb5206
miplo <miplotnova@mail.ru>
2025-02-21 17:05:48 +0500

    Homework_27

--- manage.py
+++ manage.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
@@ -16,4 +17,7 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-    print(os.environ.get('DATABASE_URL'))  # Или выведите нужные параметры вручную
+
+
+if __name__ == '__main__':
+    main()
